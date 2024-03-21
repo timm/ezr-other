40cdd485e86ca3fdf4ccc751e416d022d2bce664
Tim Menzies <timm@ieee.org>
2024-03-21 18:25:43 -0400

    saved}

--- roadmap.md
+++ roadmap.md
@@ -24,6 +24,7 @@
 - Exception handling
 - Args & Kwargs
 - lambda (closures)
+- toto: compelte this from ase24/docs/ninjas
 
 ## Just enough statistics
 
--- src/lib.lua
+++ src/lib.lua
@@ -1,70 +1,75 @@
-local l={}
-
-
+local lib={}
 -------------------------------------------------------------------------------
 -- ## Strings
 
 -- Prune leading and trailing blanks
-function l.trim(s) return s:match"^%s*(%S+)" end
+function lib.trim(s) return s:match"^%s*(%S+)" end
 -------------------------------------------------------------------------------
 -- ## Tables
 
 -- Return keys, sorted.
-function l.keys(t,    u)
+function lib.keys(t,    u)
   u={}; for k,_ in pairs(t) do u[1+#u]=k end; table.sort(u); return u end
 -------------------------------------------------------------------------------
 -- ## Coerce Strings to things
 
 -- Coerce a string to some thing.
-function l.coerce(s)
-  s = l.trim(s)
+function lib.coerce(s)
+  s = lib.trim(s)
   return math.tointeger(s) or tonumber(s) or s=="true" or (s~="false" and s) end
 
 -- Coerce `the` from `help`.
-function l.settings(s,    t)
+function lib.settings(s,    t)
   t={}
-  for k,s1 in s:gmatch("[-][-]([%S]+)[^=]+=[%s]*([%S]+)") do t[k] = l.coerce(s1) end
+  for k,s1 in s:gmatch("[-][-]([%S]+)[^=]+=[%s]*([%S]+)") do t[k] = lib.coerce(s1) end
   return t end
 
 -- Coerce rows of a csv file to LUA tables
-function l.csv(src)
+function lib.csv(src)
   src = src=="-" and io.stdin or io.input(src)
   return function(      s,t)
     s=io.read()
     if   s 
-    then t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=l.coerce(s1) end; return t
+    then t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=lib.coerce(s1) end; return t
     else io.close(src) end end end
 
 -- Coerce strings from command-line in order to update table.   
 -- Non-booleans need values but booleans just flip defaults.
-function l.cli(t)
+function lib.cli(t)
   for k, v in pairs(t) do
     v = tostring(v)
     for argv,s in pairs(arg) do
       if s=="-"..(k:sub(1,1)) or s=="--"..k then
         v = v=="true" and "false" or v=="false" and "true" or arg[argv + 1]
-        t[k] = l.coerce(v) end end end end
+        t[k] = lib.coerce(v) end end end end
 -------------------------------------------------------------------------------
 -- ## Main functions
 
 -- Is this script controlling the application?
-function l.isMain() return not pcall(debug.getlocal, 5, 1) end
+function lib.isMain() return not pcall(debug.getlocal, 5, 1) end
 
--- Run all examples
-function l.main(the,eg,help,     bads)
-  l.cli(the)
-  if the.help then os.exit(print(help)) end
+-- Run  one of more  examples.   
+-- (1) Update `the` from command line.   
+-- (2) When requested, print help and quit.   
+-- (3) Find relevant keys in `eg`.    
+-- (4) Run that example. If it returns false, increment `bads`.     
+-- (5) If running all, show final `bads` count.   
+-- (6) Return the `bads` count to the operating system
+function lib.main(the,eg,help,     bads)
+  lib.cli(the)
+  if the.help then os.exit(print(help)) end -- (1)
   bads = 0
-  for _,k in pairs(l.keys(eg)) do
-    if (the.todo == k or the.todo=="all") and l.sandbox(the, eg[k])==false then
-      io.stderr:write(l.fmt("# !!!!!!!! FAIL [%s]\n", k ))
-      bads = bads+1 end end
-  if bads>0 and the.todo=="all" then 
-    io.stderr:write(l.fmt("# !!!!!!!! FAIL(s) = %s\n",bads)) end
-  os.exit(bads) end
+  for _,k in pairs(lib.keys(eg)) do
+    if (the.todo == k or the.todo=="all") then -- (2)
+      if lib.sandbox(the, eg[k])==false then 
+        io.stderr:write(lib.fmt("# !!!!!!!! FAIL [%s]\n", k ))
+        bads = bads+1 end end end -- (3)
+  if bads>0 and the.todo=="all" then  -- (4)
+    io.stderr:write(lib.fmt("# !!!!!!!! FAIL(s) = %s\n",bads)) end
+  os.exit(bads) end  -- (5)
 
 -- Run `fun`, ensuring the config is set to defaults before and after.
-function l.sandbox(the,fun,     old,status)
+function lib.sandbox(the,fun,     old,status)
   old={}; for k,v in pairs(the) do old[k]=v end
   math.randomseed(the.seed or 1234567891) -- set up
   status = fun()
@@ -74,4 +79,4 @@ function l.sandbox(the,fun,     old,status)
 -------------------------------------------------------------------------------
 -- Return this module.
 
-return l
+return lib
